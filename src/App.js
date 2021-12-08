45d397abf771bdcc08cf0b27ec000de79299dc3a
yvethernandez <yvet_mayresa@hotmail.com>
2021-12-08 14:48:05 -0600

    first react webpack and mui

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Demo from './Demo';
+
+ 
+function App() {
+  return (
+    <div className="App">
+      <Demo/>
+     
+    </div>
+    
+  );
+}
+
+export default App;
+
---
+++ src/Demo.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function Demo() {
+  return <Button variant="contained">Bienvenidos</Button>;
+}
+
+
+export default Demo;
